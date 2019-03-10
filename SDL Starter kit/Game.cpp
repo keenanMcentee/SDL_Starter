5b7063cbdf4cfea274e722e83a0c25e359c57e8d
Krammy <keenandavidmoloney@outlook.com>
2019-03-10 18:05:17 +0000

    Tutorial 04 - Keyboard Controls

--- SDL Starter kit/Game.cpp
+++ SDL Starter kit/Game.cpp
@@ -27,7 +27,27 @@ void Game::run()
 			{
 				gameloop = false;
 			}
-
+			else if (e.type == SDL_KEYDOWN)
+			{
+				switch (e.key.keysym.sym)
+				{
+				case SDLK_UP:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_UP];
+					break;
+				case SDLK_DOWN:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_DOWN];
+					break;
+				case SDLK_LEFT:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT];
+					break;
+				case SDLK_RIGHT:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_LEFT];
+					break;
+				default:
+					currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+					break;
+				}
+			}
 		}
 		update();
 		draw();
@@ -44,10 +64,13 @@ void Game::initialise()
 	gameloop = true;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
-	window = SDL_CreateWindow("Tutorial 2 & 3 Images & Events", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-	gScreenSurface = SDL_GetWindowSurface(window);
+	window = SDL_CreateWindow("Tutorial 04 Keyboard Control", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	screenSurface = SDL_GetWindowSurface(window);
 
 	loadMedia();
+
+	currentSurface = keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+
 }
 
 /// <summary>
@@ -63,7 +86,7 @@ void Game::update()
 /// </summary>
 void Game::draw()
 {
-	SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+	SDL_BlitSurface(currentSurface, NULL, screenSurface, NULL);
 	SDL_UpdateWindowSurface(window);
 }
 
@@ -72,20 +95,31 @@ void Game::draw()
 
 void Game::loadMedia()
 {
-	gHelloWorld = SDL_LoadBMP("./img/hello_world.bmp");
-	if (gHelloWorld == NULL) 
+	keyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT] = loadSurface("./img/press.bmp");
+	keyPressSurfaces[KEY_PRESS_SURFACE_UP] = loadSurface("./img/up.bmp");
+	keyPressSurfaces[KEY_PRESS_SURFACE_DOWN] = loadSurface("./img/down.bmp");
+	keyPressSurfaces[KEY_PRESS_SURFACE_LEFT] = loadSurface("./img/left.bmp");
+	keyPressSurfaces[KEY_PRESS_SURFACE_RIGHT] = loadSurface("./img/right.bmp");
+}
+
+SDL_Surface* Game::loadSurface(std::string path)
+{
+	SDL_Surface* loadedSurface = SDL_LoadBMP(path.c_str());
+	if (loadedSurface == NULL) 
 	{ 
-		printf("Unable to load image %s! SDL Error: %s\n", "02_getting_an_image_on_the_screen/hello_world.bmp", SDL_GetError());
+		printf("Unable to load image %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
 	}
+	return loadedSurface;
 }
 
 void Game::close()
 {
-	SDL_FreeSurface(gHelloWorld);
-	gHelloWorld = NULL;
+	//SDL_FreeSurface(gHelloWorld);
+	//gHelloWorld = NULL;
 
 	SDL_DestroyWindow(window);
 	window = NULL;
 
 	SDL_Quit();
 }
+
--- SDL Starter kit/Game.h
+++ SDL Starter kit/Game.h
@@ -12,15 +12,31 @@ public:
 	void draw();
 
 	void loadMedia();
+	SDL_Surface* loadSurface(std::string path);
 	void close();
 
 	//While true, program keeps running
 	bool gameloop;
 
+	enum KeyPressSurfaces
+	{
+		KEY_PRESS_SURFACE_DEFAULT,
+		KEY_PRESS_SURFACE_UP,
+		KEY_PRESS_SURFACE_DOWN,
+		KEY_PRESS_SURFACE_LEFT,
+		KEY_PRESS_SURFACE_RIGHT,
+		KEY_PRESS_SURFACE_TOTAL
+	};
+
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
 
-	SDL_Surface *gScreenSurface = NULL;
-	SDL_Surface *gHelloWorld = NULL;
+	SDL_Surface *screenSurface = NULL;
+	SDL_Surface *keyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+	SDL_Surface *currentSurface = NULL;
+
+
+
+	
 };
 
